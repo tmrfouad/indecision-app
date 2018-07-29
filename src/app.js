1b9ccc38bd5211ea40fb6ba12211803787fce694
Tamer Fouad Abuhmead <tmrfouad@gmail.com>
2018-07-30 00:20:19 +0200

    2018-07-30 12:20

--- src/app.js
+++ src/app.js
@@ -41,8 +41,14 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
+
     handleRemoveAll() {
-        alert('removeAll');
+        console.log(this.props.options);
+        // alert('removeAll');
     }
 
     render() {
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,33 +1,82 @@
-//------ template two ------
-const appRoot = document.getElementById('app');
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.plusOne = this.plusOne.bind(this);
+        this.minusOne = this.minusOne.bind(this);
+        this.reset = this.reset.bind(this);
+        this.state = {
+            count: 0
+        }
+    }
 
-let count = 0;
-const plusOne = () => {
-    count++;
-    render();
-};
-const minusOne = () => {
-    count--;
-    render();
-};
-const reset = () => {
-    count = 0;
-    render();
-};
+    plusOne() {
+        this.setState(prevState => {
+            return {
+                count: ++prevState.count
+            };
+        });
+    }
 
-const renderApp = () => {
-    // JSX Javascript XML
-    const template = (
-        <div>
-            <h1>Count : {count}</h1>
-            <button onClick={plusOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    );
+    minusOne() {
+        this.setState(prevState => {
+            return {
+                count: --prevState.count
+            };
+        });
+    }
 
-    // render JSX template
-    ReactDOM.render(template, appRoot);
+    reset() {
+        this.setState(() => {
+            return {
+                count: 0
+            };
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.plusOne}>+1</button>
+                <button onClick={this.minusOne}>-1</button>
+                <button onClick={this.reset}>Reset</button>
+            </div>
+        );
+    }
 }
 
-render();
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+// //------ template two ------
+// const appRoot = document.getElementById('app');
+
+// let count = 0;
+// const plusOne = () => {
+//     count++;
+//     render();
+// };
+// const minusOne = () => {
+//     count--;
+//     render();
+// };
+// const reset = () => {
+//     count = 0;
+//     render();
+// };
+
+// const render = () => {
+//     // JSX Javascript XML
+//     const template = (
+//         <div>
+//             <h1>Count : {count}</h1>
+//             <button onClick={plusOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>Reset</button>
+//         </div>
+//     );
+
+//     // render JSX template
+//     ReactDOM.render(template, appRoot);
+// }
+
+// render();
